7a15b4ec7231353eb088d98c1dcf5a6472448cc1
NExPlain <lizhenpi@gmail.com>
2015-07-03 16:23:18 +0800

    add get_list API

--- controller/session.js
+++ controller/session.js
@@ -46,6 +46,74 @@ var detail = function(req, res, next) {
         });
 };
 
+var list = function(req, res, next){
+    var query_dispatcher_user_name = req.query.dispatcherUserName;
+    var query_worker_user_name = req.query.workerUserName;
+    var query_readonly = req.query.readonly;
+    var query_reviewer_user_name = req.query.reviewer;
+    var find_key = {
+        status: req.query.status,
+        isRejected: req.query.isRejected,
+        isRedirected: req.query.isRedirected
+    }
+    if(query_readonly){
+        find_key.readonly = query_readonly
+    }
+    if(query_dispatcher_user_name) {
+        find_key.dispatcher = User.model.findOne({
+            username: query_dispatcher_user_name,
+            role: User.Role.Dispatcher
+        })._id
+    }
+    if(query_worker_user_name){
+        find_key.worker = User.model.findOne({
+            username: query_worker_user_name,
+            role: User.Role.Worker
+        })._id
+    }
+    if(query_reviewer_user_name){
+        find_key.reviewer = User.model.findOne({
+            username: query_reviewer_user_name,
+            role: User.Role.Reviewer
+        })._id
+    }
+    Session.model.find(find_key)
+        .populate('dispatcher', 'username')
+        .populate('worker', 'username')
+        .populate('reviewer', 'username')
+        .populate('income')
+        .exec(function(err, sessions){
+            if (err) {
+                return res.json({
+                    code: 123,
+                    message: 'asdf'
+                });
+            }
+            ret.count = sessions.length;
+            sessions.forEach(function (session, index){
+                var list_element = {
+                    id: session.id,
+                    readonly: session.readonly,
+                    dispatcher: session.dispatcher.username,
+                    worker: session.worker.username,
+                    reviewer: session.reviewer.username,
+                    status: session.status,
+                    isRejected: session.isRejected,
+                    isRedirected: session.isRedirected,
+                    income: {
+                        subject: session.income.subject,
+                        from: session.income.from,
+                        labels: session.income.labels,
+                        deadline: session.income.deadline
+                    }
+                };
+                ret.sessions.push(list_element);
+            });
+            res.json(ret);
+    })
+}
+
+
 router.use(function(req, res, next) {
     if(!req.session.user) {
         return res.json({
@@ -65,5 +133,6 @@ router.use(function(req, res, next) {
     });
 });
 router.route('/get_detail').get(detail);
+router.route('/get_list').get(list);
 
 exports = module.exports = router;
