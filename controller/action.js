b7ee0cf8fe97bf345caea9582f3c72fe18233488
Renfei Song <renfei.song@gmail.com>
2015-07-03 19:27:10 +0800

    implemented worker_submit and reviewer_pass interface

--- controller/action.js
+++ controller/action.js
@@ -71,11 +71,110 @@ var dispatcher_dispatch = function(req, res, next) {
 };
 
 var worker_submit = function(req, res, next) {
+    var sessionId = req.body.id;
+    var userId = req.session.user;
+    var subject = req.body.subject;
+    var html = req.body.html;
+    var needReview = req.body.needReview;
+    var reviewerUsername = req.body.reviewer;
 
+    var user, reviewer, session, incomeMail;
+
+    async.series([
+        function(callback) {
+            User.model.findById(mongoose.Types.ObjectId(userId), function(err, _user) {
+                user = _user;
+                callback(err, 'get operating user');
+            });
+        }, function(callback) {
+            User.model.find({ username: reviewerUsername }, function(err, _reviewer) {
+                reviewer = _reviewer;
+                callback(err, 'get designated reviewer');
+            });
+        }, function(callback) {
+            Session.model.findById(mongoose.Types.ObjectId(sessionId), function(err, _session) {
+                session = _session;
+                callback(err, 'get session');
+            });
+        }, function(callback) {
+            Mail.model.findById(mongoose.Types.ObjectId(session.income), function(err, _incomeMail) {
+                incomeMail = _incomeMail;
+                callback(err, 'get income mail');
+            });
+        }
+    ], function() {
+        var repliedMail = new Mail.model({
+            to: [{
+                address: incomeMail.to[0].address,
+                name: incomeMail.to[0].name
+            }],
+            subject: subject,
+            html: html,
+            time: new Date()
+        });
+        var operationDict = {
+            type: needReview ? 3 : 6,
+            operator: user._id,
+            time: new Date(),
+            mail: repliedMail._id
+        };
+        if (needReview) {
+            operationDict['receiver'] = reviewer._id;
+        }
+        session.status = needReview ? 2 : 3;
+        session.reply = repliedMail._id;
+        session.reviewer = reviewer._id;
+        session.operations.push(operationDict);
+
+        res.json({
+            "code": 0,
+            "message": "Success"
+        });
+    });
 };
 
 var reviewer_pass = function(req, res, next) {
+    var sessionId = req.body.id;
+    var userId = req.session.user;
+    var user, session, mail;
 
+    async.series([
+        function(callback) {
+            User.model.findById(mongoose.Types.ObjectId(userId), function(err, _user) {
+                user = _user;
+                callback(err, 'get operating user');
+            });
+        },
+        function(callback) {
+            Session.model
+                .findById(mongoose.Types.ObjectId(sessionId))
+                .populate('reply', 'reply')
+                .exec(function(err, _session) {
+                session = _session;
+                callback(err, 'get session');
+            });
+        },
+        function(callback) {
+            Mail.model.findById(mongoose.Types.ObjectId(session.reply), function(err, _mail) {
+                mail = _mail;
+                callback(err, 'get mail');
+            });
+        }
+    ],function() {
+        var operationDict = { 
+            type: 5, 
+            operator:  user._id,
+            time: new Date(),
+            mail: mail._id
+        };
+        session.status = 3;
+        session.operations.push(operationDict);
+
+        res.json({
+            "code": 0,
+            "message": "Success"
+        });
+    });
 };
 
 router.use(function(req, res, next) {
