0e54acaf65c0a7178bf97f25d80642ed8745a52d
liuxiaoran <lxr_buaa_phd@buaa.edu.cn>
2015-07-04 23:32:16 +0800

    修改邮件正文controll

--- web/controllers/MailController.js
+++ web/controllers/MailController.js
@@ -1,7 +1,7 @@
 SRFMailProControllers.controller("MailController", ["$scope", "$http", "$cookies",
     function ($scope, $http, $cookies) {
-        $scope.mail_title = "hello liuxiaran";
-        $scope.time = "1:20";
-        $scope.sender = "fly@163.com";
-        $scope.content="adfadf";
+        //$scope.mail_title = $scope.selected_mail.subject;
+        //$scope.time = $scope.selected_mail.time.substr(0,10);
+        //$scope.sender = $scope.selected_mail.from.address;
+        //$scope.content=$scope.selected_mail.html;
     }]);
