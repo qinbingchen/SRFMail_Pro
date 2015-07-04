c5105045bc04b7bedc7e1766402be981b4d3674d
shushuInBuaa <shushuInBuaa@users.noreply.github.com>
2015-07-04 22:55:32 +0800

    Update MailListController.js

--- web/controllers/MailListController.js
+++ web/controllers/MailListController.js
@@ -1,5 +1,14 @@
 SRFMailProControllers.controller("MailListController", ["$scope", "$http", "$cookies",
     function ($scope, $http, $cookies) {
 
+        $scope.selectMail = function(mail){
+            selected_mail = mail;
+        };
 
-    }]);
+        $scope.changeClass=function(mail){
+            if(mail.sender==selected_mail.sender)
+                return 'mail selected';
+            else
+                return 'mail';
+        }
+        }]);
