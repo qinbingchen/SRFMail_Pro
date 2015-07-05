e795be2bfb2ebdc6a427f23f30e4ba1233adf1b8
Yu Zhan <me@yuzhan.info>
2015-07-05 14:22:35 +0800

    调整log位置

--- controller/index.js
+++ controller/index.js
@@ -1,5 +1,9 @@
 var router = new require('express').Router();
 
+router.use(function(req, res, next) {
+    Log.d({req: req});
+    next();
+});
 router.use('/user', require('./user'));
 router.use('/session', require('./session'));
 router.use('/action', require('./action'));
