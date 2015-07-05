70555e1c58de4f8536cdc75c1f88fc562eafac10
qinbingchen <qinbingchen@me.com>
2015-07-05 14:07:14 +0800

    Update.

--- web/controllers/MailController.js
+++ web/controllers/MailController.js
@@ -3,15 +3,6 @@ SRFMailProControllers.controller("MailController", ["$scope", "$http", "$cookies
         $scope.partial_load_status.mail = true;
         $scope.check_partial_load_status();
 
-        //$scope.mail_title = "hello liuxiaran";
-        //$scope.time = "1:20";
-        //$scope.sender = "fly@163.com";
-        //$scope.content="adfadf";
-        //$scope.mail_title = $scope.selected_mail.subject;
-        //$scope.time = $scope.selected_mail.time.substr(0,10);
-        //$scope.sender = $scope.selected_mail.from.address;
-        //$scope.content=$scope.selected_mail.html;
-
         $scope.$on("broadcast_mail_selected", function () {
             console.log("received mail selected");
             console.log($scope.$parent.$parent.selected_mail);
