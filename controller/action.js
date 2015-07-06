9cb3507699dd7f7af2496d6eeecfdbe74f150eb4
Yu Zhan <me@yuzhan.info>
2015-07-06 20:18:51 +0800

    添加上传文件接口，更新package.json

--- controller/action.js
+++ controller/action.js
@@ -3,5 +3,6 @@ var router = new require('express').Router();
 router.use('/dispatcher', require('./dispatcher'));
 router.use('/reviewer', require('./reviewer'));
 router.use('/worker', require('./worker'));
+router.use('/upload', require('./upload'));
 
 module.exports = router;
