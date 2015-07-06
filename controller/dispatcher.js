32c31114fc9e4b3ea2d3359dcd719fd43e399af6
Renfei Song <renfei.song@gmail.com>
2015-07-06 19:57:00 +0800

    fixed dispatch API

--- controller/dispatcher.js
+++ controller/dispatcher.js
@@ -14,13 +14,30 @@ var Log = require('../lib/log')('[controller-session]');
 var dispatch = function(req, res, next) {
     var sessionId = req.body.id;
     var readonlyWorkers, readreplyWorkers;
+    var deadline;
+    if (req.body.deadline) {
+        deadline = new Date(req.body.deadline);
+        if (deadline.toString() == 'Invalid Date') {
+            return res.json({
+                code: 1,
+                message: 'Error: Invalid date string ' + req.body.deadline
+            });
+        }
+        if (deadline < new Date()) {
+            return res.json({
+                code: 1,
+                message: 'Error: Designated deadline (' + deadline.toLocaleString() + ') is earlier then now (' + (new Date()).toLocaleString() + ')'
+            });
+        }
+    }
+
     try {
         readonlyWorkers = JSON.parse(req.body.readonly);
         readreplyWorkers = JSON.parse(req.body.readreply);
     } catch (e) {
         return res.json({
             code: 1,
-            message: 'Invalid JSON received, please ensure that readonly & readreply is valid JSON string representation.'
+            message: 'Error: Invalid JSON received, please ensure that readonly & readreply parameters hold valid JSON string representations.'
             + ' Reason: ' + e.toString()
             + ' ReadonlyWorkers: ' + req.body.readonly
             + ' ReadreplyWorkers: ' + req.body.readreply
@@ -28,21 +45,38 @@ var dispatch = function(req, res, next) {
     }
 
     var currentUser = req.session.user;
-    var originalSession;
+    var originalSession, incomeMail;
 
     if (!mongoose.Types.ObjectId.isValid(sessionId)) {
         return res.json({
             code: 1,
-            message: 'Invalid session ID ' + sessionId
+            message: 'Error: Invalid session ID ' + sessionId
         });
     }
 
-    async.parallel([
+    async.series([
         // populate originalSession & currentUser
         function(callback) {
             Session.model.findById(mongoose.Types.ObjectId(sessionId), function(err, _originalSession) {
                 originalSession = _originalSession;
                 callback(err, 'get original session');
+            });
+        },
+        function(callback) {
+            if (!deadline || !originalSession) {
+                callback();
+                return;
+            }
+            Mail.model.findById(mongoose.Types.ObjectId(originalSession.income), function(err, _incomeMail) {
+                incomeMail = _incomeMail;
+                if (!incomeMail) {
+                    callback(err, 'get income mail');
+                    return;
+                }
+                incomeMail.deadline = deadline;
+                incomeMail.save(function(err) {
+                    callback(err, 'update income mail');
+                });
             });
         }
     ], function(err) {
@@ -56,14 +90,21 @@ var dispatch = function(req, res, next) {
         if (!originalSession) {
             return res.json({
                 code: 1,
-                message: "Couldn't find session with ID " + sessionId
+                message: "Error: Couldn't find session with ID " + sessionId
+            });
+        }
+
+        if (deadline && !incomeMail) {
+            return res.json({
+                code: 1,
+                message: "Error: Couldn't find mail with ID " + originalSession.income + " from session with ID " + originalSession._id
             });
         }
 
         if (originalSession.status != 0) {
             return res.json({
                 code: 1,
-                message: "The session's status is " + originalSession.status + " therefore couldn't be dispatched. Aborting."
+                message: "Error: The session's status is " + originalSession.status + " therefore couldn't be dispatched. Aborting."
             });
         }
 
@@ -76,71 +117,68 @@ var dispatch = function(req, res, next) {
             }, function(err, designatedWorker) {
                 if (!designatedWorker) {
                     callback();
-                } else {
-                    effectiveWorkers.push(worker);
-                    var session = new Session.model({
-                        income: originalSession.income,
-                        dispatcher: currentUser._id,
-                        worker: designatedWorker._id,
-                        readonly: (readonlyWorkers.indexOf(worker) > -1),
-                        operations: [],
-                        status: 1,
-                        isRejected: false,
-                        isRedirected: false
-                    });
-                    originalSession.operations.forEach(function(row) {
-                        session.operations.push(row);
-                    });
-                    var operationDict = {
-                        type: 1,
-                        operator: currentUser._id,
-                        receiver: designatedWorker._id,
-                        time: new Date()
-                    };
-                    session.operations.push(operationDict);
-                    session.save(function(err) {
-                        callback(err);
-                    });
+                    return;
                 }
+                if (designatedWorker.role != User.Role.Worker) {
+                    callback();
+                    return;
+                }
+                effectiveWorkers.push(worker);
+                var session = new Session.model({
+                    income: originalSession.income,
+                    dispatcher: currentUser._id,
+                    worker: designatedWorker._id,
+                    readonly: (readonlyWorkers.indexOf(worker) > -1),
+                    operations: [],
+                    status: 1,
+                    isRejected: false,
+                    isRedirected: false
+                });
+                originalSession.operations.forEach(function(row) {
+                    session.operations.push(row);
+                });
+                var operationDict = {
+                    type: 1,
+                    operator: currentUser._id,
+                    receiver: designatedWorker._id,
+                    time: new Date()
+                };
+                session.operations.push(operationDict);
+                session.save(function(err) {
+                    callback(err);
+                });
             });
         }, function(err) {
             if (err) {
-                res.json({
+                return res.json({
                     code: 1,
                     message: err.toString()
                 });
-            } else {
-                Session.model.findByIdAndRemove(mongoose.Types.ObjectId(sessionId), function(err) {
-                    if (err) {
-                        return res.json({
-                            code: 1,
-                            message: err.toString()
-                        });
-                    } else {
-                        return res.json({
-                            code: 0,
-                            message: "Success; Workers " + effectiveWorkers.join(", ") + " designated."
-                        });
-                    }
+            }
+
+            if (effectiveWorkers.length == 0) {
+                return res.json({
+                    code: 1,
+                    message: "Error: None of the workers you designated is valid (a user is" +
+                    " considered a valid worker if and only if *all* of the following criterias" +
+                    " are met: 1) the user can be found in our database; 2) the user is a worker"
                 });
             }
+
+            Session.model.findByIdAndRemove(mongoose.Types.ObjectId(sessionId), function(err) {
+                if (err) {
+                    return res.json({
+                        code: 1,
+                        message: err.toString()
+                    });
+                }
+                return res.json({
+                    code: 0,
+                    message: "Success: Workers " + effectiveWorkers.join(", ") + " designated."
+                });
+            });
         });
     });
-};
-
-var set_deadline = function(req, res, next) {
-    var sessionId = req.body.id;
-    var deadline = req.body.deadline;
-    var deadlineDate = new Date(deadline);
-    if (deadlineDate.toString() == 'Invalid Date') {
-        return res.json({
-            code: 1,
-            message: 'Invalid date ' + deadline
-        });
-    }
-
-    var currentUser = req.session.user;
-    
 };
 
 var urge = function(req, res, next) {
@@ -151,21 +189,21 @@ router.use(function(req, res, next) {
     if (!req.session.user) {
         return res.json({
             code: 1,
-            message: 'You are not yet logged in'
+            message: 'Error: You are not yet logged in'
         });
     }
     User.model.findById(mongoose.Types.ObjectId(req.session.user._id), function(err, user) {
         if (err || !user) {
             return res.json({
                 code: 1,
-                message: "Couldn't find user with ID " + req.session.user._id
+                message: "Error: Couldn't find user with ID " + req.session.user._id
             });
         }
         req.session.user = user;
         if (user.role != User.Role.Dispatcher) {
             return res.json({
                 code: 1,
-                message: "Unauthorized: User " + user.username + " with role " + user.role + " isn't a dispatcher"
+                message: "Error: Unauthorized: User " + user.username + " with role " + user.role + " isn't a dispatcher"
             });
         }
         next();
@@ -173,6 +211,5 @@ router.use(function(req, res, next) {
 });
 
 router.route('/dispatch').post(dispatch);
-router.route('/set_deadline').post(set_deadline);
 router.route('/urge').post(urge);
 module.exports = router;
