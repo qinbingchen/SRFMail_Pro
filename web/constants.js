6dd1dc7a5d688b4f975c9cc6e23656870c79bf4d
liuxiaoran <lxr_buaa_phd@buaa.edu.cn>
2015-07-08 16:29:37 +0800

    增加hint

--- web/constants.js
+++ web/constants.js
@@ -40,10 +40,12 @@ var CATEGORY_LIST = [
         category: [
             {
                 name: "pending",
-                display_name: "待分发"
+                display_name: "待分发",
+                hint: "表示仍未被分发的邮件列表"
             }, {
                 name: "dispatched",
-                display_name: "已分发"
+                display_name: "已分发",
+                hint: "表示已经分发完但尚未被处理人员处理的邮件列表"
             }, {
                 name: "processed",
                 display_name: "已处理"
@@ -54,16 +56,20 @@ var CATEGORY_LIST = [
         category: [
             {
                 name: "pending",
-                display_name: "未处理"
+                display_name: "未处理",
+                hint: "表示未处理的邮件列表"
             }, {
                 name: "rejected",
-                display_name: "未通过审核"
+                display_name: "未通过审核",
+                hint: "表示被审核人员拒绝并退回的邮件列表"
             }, {
                 name: "waiting_for_review",
-                display_name: "已提交审核"
+                display_name: "已提交审核",
+                hint: "表示已提交审核但审核人员未处理的邮件列表"
             }, {
                 name: "success",
-                display_name: "已处理"
+                display_name: "已处理",
+                hint: "表示已标记成已处理或已成功发送的邮件列表"
             }
         ]
     }, {
@@ -71,13 +77,16 @@ var CATEGORY_LIST = [
         category: [
             {
                 name: "pending",
-                display_name: "未审核"
+                display_name: "未审核",
+                hint: "表示尚未被审核的邮件列表"
             }, {
                 name: "sent",
-                display_name: "已发送"
+                display_name: "已通过",
+                hint: "表示通过审核的邮件列表"
             }, {
                 name: "rejected",
-                display_name: "已退回"
+                display_name: "已拒绝",
+                hint: "表示已拒绝但尚未被处理人员处理的邮件列表"
             }
         ]
     }
