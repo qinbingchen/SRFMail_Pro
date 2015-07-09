c0cf0a85c216cb1e44d036ff147607345f7c12ed
Yu Zhan <me@yuzhan.info>
2015-07-09 11:42:27 +0800

    修改一个错误

--- model/user.js
+++ model/user.js
@@ -7,7 +7,7 @@ var UserSchema = new schema({
     name: { type: String },
     gender: { type: Number },
     role: { type: Number },
-    defaultReviewer: { type: String }
+    defaultReviewer: { type: Schema.ObjectId, ref: 'User' }
 });
 
 exports.model = mongoose.model('User', UserSchema);
