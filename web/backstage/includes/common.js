405423cc77106c6c97c7159dcd86d47d53bd384f
Renfei Song <renfei.song@gmail.com>
2015-07-09 13:39:11 +0800

    Add SRFMail Pro SmartBackstage(TM) (use /backstage to enter)

---
+++ web/backstage/includes/common.js
@@ -0,0 +1,44 @@
+var SB_HOST = 'http://localhost:8844'; // SmartBackstage Host
+
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "positionClass": "toast-top-right",
+    "onclick": null,
+    "showDuration": "1000",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+};
+
+$(document).ready(function() {
+    $('input').iCheck({
+        checkboxClass: 'icheckbox_minimal-grey',
+        radioClass: 'iradio_minimal-grey'
+    });
+
+    $('select').select2();
+
+    var maxVal = function(a, b) {
+        return a > b ? a : b;
+    };
+
+    var resizeSiteContent = function() {
+        $('.site-content')[0].style.minHeight = maxVal($('.site-sidebar').height(), $(window).height() - 84) + 'px';
+    };
+    resizeSiteContent();
+    $(window).on('resize', resizeSiteContent);
+
+    $('#log-out').click(function() {
+        $.ajax(SB_HOST + '/api/user/logout', {
+            method: 'POST',
+            complete: function() {
+                window.location = 'login.html';
+            }
+        })
+    });
+});
