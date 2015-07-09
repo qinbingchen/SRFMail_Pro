b125d6f2c512f4198896e144da72ab3d6d6fb675
qinbingchen <qinbingchen@me.com>
2015-07-09 09:48:51 +0800

    Add popover.

--- web/controllers/SelectWorkerController.js
+++ web/controllers/SelectWorkerController.js
@@ -1,71 +1,67 @@
-SRFMailProControllers.controller("SelectWorkerController", ["$scope", "$http", "$cookies",
+SRFMailProControllers.controller("PopoverDispatchController", ["$scope", "$http", "$cookies",
     function ($scope, $http, $cookies) {
 
-           $scope.sendDispatch=function(){
+       $scope.sendDispatch = function () {
 
-            var readreply_selected = $("#readreply").select2("val");
-            if(readreply_selected!=null)
+        var readreply_selected = $("#readreply").select2("val");
+        if (readreply_selected != null) {
+            var readreply="[\""+readreply_selected[0]+"\"";
+            for(i=1;i<readreply_selected.length;i++)
             {
-                var readreply="[\""+readreply_selected[0]+"\"";
-                for(i=1;i<readreply_selected.length;i++) 
-                {
-                    readreply+=",\""+readreply_selected[i]+"\"";
-                }
-                readreply+="]";
+                readreply+=",\""+readreply_selected[i]+"\"";
             }
-            else
-            {
-                readreply="[]";
-            }
+            readreply+="]";
+        }
+        else
+        {
+            readreply="[]";
+        }
 
-            var readonly_selected = $("#readonly").select2("val");
-            if(readonly_selected!=null)
+        var readonly_selected = $("#readonly").select2("val");
+        if(readonly_selected!=null)
+        {
+            var readonly="[\""+readonly_selected[0]+"\"";
+            for(i=1;i<readonly_selected.length;i++)
             {
-                var readonly="[\""+readonly_selected[0]+"\"";
-                for(i=1;i<readonly_selected.length;i++) 
-                {
-                    readonly+=",\""+readonly_selected[i]+"\"";
-                }
-                readonly+="]";
+                readonly+=",\""+readonly_selected[i]+"\"";
             }
-            else
-            {
-                readonly="[]";
-            }
-            
-            var deadline = $("#deadline").val();           
+            readonly+="]";
+        }
+        else
+        {
+            readonly="[]";
+        }
 
-             var url = "/api/action/dispatcher/dispatch";
-            $http.post(url, {
-                id: $scope.$parent.$parent.selected_mail_id,
-                readonly: readonly,
-                readreply: readreply,
-                deadline: deadline
-            }).success(function (data, status, headers, config) {
+        var deadline = $("#deadline").val();
+
+        $http.post("/api/action/dispatcher/dispatch", {
+            id: $scope.$parent.$parent.selected_mail_id,
+            readonly: readonly,
+            readreply: readreply,
+            deadline: deadline
+        }).success(function (data, status, headers, config) {
+            if (data.code == 0) {
+                location.reload();
+            } else {
+                console.log(data);
+            }
+        }).error(function (data, status, headers, config) {
+            console.log(data);
+        });
+    };
+
+        $http.get("/api/user/list_workers")
+            .success(function (data) {
                 if (data.code == 0) {
-                    location.reload();
+                    $scope.worker_list = data.workers;
                 } else {
                     console.log(data);
                 }
             }).error(function (data, status, headers, config) {
                 console.log(data);
-            });            
-        };
+            });
 
-        var url_workers = "/api/user/list_workers";
-        $http.get(url_workers).success(function (data) {
-                $scope.workers=data.workers;
-           
-        }).error(function (data, status, headers, config) {
-            console.log(data);
-    });
-
-            	setTimeout(function() {
-               $(".select-workers").select2({
-                    data: $scope.workers
-                });              
-        }, 2000);  
-
-    }]);
+    }
+]);
 
 
